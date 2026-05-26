c22279c479fb22f005c840ca53d5bba578b34b36
PrudhviNadh Pemmasani <pemmasaniprudhvi7108@gmail.com>
2026-05-26 13:51:11 -0400

    paymentJira-05 - Added validation of client request with authentication and authorization at API gateway level.

--- api-gateway/src/main/java/com/example/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/gateway/ApiGatewayApplication.java
@@ -2,8 +2,12 @@ package com.example.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableScheduling
 public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
